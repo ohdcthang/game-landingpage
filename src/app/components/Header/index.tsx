1922c8821f9fde04c6321cddd8c5cf77bd9c019d
thanghoduc <hoducthangtn2nhvt@gmail.com>
2024-07-15 00:33:17 +0700

    u

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,10 +1,14 @@
+'use client'
+
 import React from 'react'
 import { Icon } from '../Icon'
 import logo from '../../asset/img/logo.png'
 import Image from 'next/image'
+import { Fade } from 'react-awesome-reveal'
 
 export const Header = () => {
   return (
+    <Fade  direction='down' duration='100'>
     <div className='h-[60px] bg-black bg-opacity-60 fixed top-0 right-0 left-0 flex justify-between items-center px-4 lg:px-12 text-lg text-white font-semibold '>
       <div className='shadow-lg'>
        <Image src={logo} width={180}/>
@@ -23,5 +27,7 @@ export const Header = () => {
         </div>
       </div>
     </div>
+    </Fade>
+
   )
 }
